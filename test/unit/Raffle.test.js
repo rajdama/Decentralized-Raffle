350e6f2f4bd5c1f7a985b6250bec84af0df2ca3b
raj <rajdama1728@gmail.com>
2023-06-26 11:28:58 +0530

    unit test for checkupkeep completed

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -94,5 +94,23 @@ const { assert, expect } = require("chai");
           assert.equal(raffleState.toString(), "1");
           assert.equal(upkeepNeeded, false);
         });
+        it("returns false if enough time hasn't passed", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() - 5,
+          ]); // use a higher number here if this test fails
+          await network.provider.request({ method: "evm_mine", params: [] });
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x"); // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+          assert(!upkeepNeeded);
+        });
+        it("returns true if enough time has passed, has players, eth, and is open", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.request({ method: "evm_mine", params: [] });
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x"); // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+          assert(upkeepNeeded);
+        });
       });
     });
